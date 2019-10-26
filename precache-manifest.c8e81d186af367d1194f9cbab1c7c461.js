5e79956b2e42ba4a052101e957706e468c9e2c6e
John Ratcliff <john.ratcliff@dockyard.com>
2019-10-25 22:21:01 -0700

    Updates

---
+++ precache-manifest.c8e81d186af367d1194f9cbab1c7c461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36acb8b0a150322ae6dfff02112ec26b",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "0250c384291058129fe0",
+    "url": "/minesweeper/static/css/main.61133409.chunk.css"
+  },
+  {
+    "revision": "3406c2bec0d35085b70e",
+    "url": "/minesweeper/static/js/2.5c9c0449.chunk.js"
+  },
+  {
+    "revision": "0250c384291058129fe0",
+    "url": "/minesweeper/static/js/main.f41118df.chunk.js"
+  },
+  {
+    "revision": "546a098e25c3db089e5d",
+    "url": "/minesweeper/static/js/runtime-main.a7fec516.js"
+  }
+]);
